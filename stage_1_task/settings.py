e1ea7d5fcf7ab0d014f58a0dc6cf4ea99966d3ae
Eyitayo Olonilua <oloniluaeyitayo@gmail.com>
2022-10-29 11:34:49 -0700

    l

--- stage_1_task/settings.py
+++ stage_1_task/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["desolate-dawn-29749.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
+
+# ALLOWED_HOSTS = ["desolate-dawn-29749.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
