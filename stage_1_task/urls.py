36d2383dc4e293fbc421cfd5fe8a19008881ffb6
Eyitayo Olonilua <oloniluaeyitayo@gmail.com>
2022-10-29 10:06:11 -0700

    in

--- stage_1_task/urls.py
+++ stage_1_task/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('info/', views.info)
+    path('', views.info)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
  
